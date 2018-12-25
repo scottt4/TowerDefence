27ac20600168e8eaa69bdfdb6427c64eee8ab3f4
scottt4 <31971317+scottt4@users.noreply.github.com>
2018-12-25 01:00:36 -0500

    Added BoardManager
    Added board manager to generate our board. Necessary for the next portion: enemy movement, attacks, and tower health

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Public Properties
+    public GameObject[] backgroundTiles;
+    public GameObject[] floorTiles;
+    public GameObject[] foregroundTiles;
+    public GameObject[] enemies;
+    public GameObject exit;
+
+    public List<Vector3> createdBackground;
+    public List<Vector3> createdFloor;
+    public List<Vector3> createdForeground;
+    public List<Vector3> createdEnemies;
+
+    public float rockTileSize;
+    public float rockBorderSize;
+
+    #endregion
+
+
+    #region Private Properties
+    private static int Width = 45;
+    private static int Height = 45;
+    private int RockHeight = Height / 6;
+    #endregion
+
+    void Start()
+    {
+        StartCoroutine(GenerateBoard());
+    }
+
+    #region Generate Level
+    IEnumerator GenerateBoard()
+    {
+        #region Generate Rocks Floor
+        for (int j = 0; j < RockHeight; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                transform.position = new Vector3(rockTileSize * i, rockTileSize * j, 0);
+                CreateRockFloor();
+            }
+        }
+        #endregion
+
+        #region Generate Rocks Border
+        for (int i = 0; i < Width; i++)
+        {
+            transform.position = new Vector3(rockTileSize * i, 0, 0);
+            CreateRockBorder();
+        }
+
+        for (int i = 0; i < Width; i++)
+        {
+            transform.position = new Vector3(rockTileSize * i, rockTileSize * (RockHeight - 1), 0);
+            CreateRockBorder();
+        }
+        #endregion
+        yield return 0;
+    }
+
+    #region Create Tiles
+    void CreateRockFloor()
+    {
+        GameObject rockFloor;
+        rockFloor = Instantiate(floorTiles[0], transform.position, transform.rotation) as GameObject;
+
+        createdFloor.Add(rockFloor.transform.position);
+    }
+
+    void CreateRockBorder()
+    {
+        GameObject rockBorder;
+        rockBorder = Instantiate(floorTiles[1], transform.position, transform.rotation) as GameObject;
+
+        createdFloor.Add(rockBorder.transform.position);
+    }
+    #endregion
+    #endregion
+}
